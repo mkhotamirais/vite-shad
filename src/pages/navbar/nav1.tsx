e6645173203003552c2875505734b8af3c8c0989
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-21 21:53:56 +0700

    sedikit

--- src/pages/navbar/nav1.tsx
+++ src/pages/navbar/nav1.tsx
@@ -1,5 +1,3 @@
-// import {motion} from 'framer-motion'
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
@@ -14,10 +12,11 @@ const navMenu = [
 export default function Nav1() {
   const [activeHover, setActiveHover] = useState("");
   const [activeClick, setActiveClick] = useState("home");
+
   return (
     <section>
       <div className="container">
-        <div className="border flex items-center h-16">
+        <div className="flex items-center h-16">
           {navMenu.map((item, i) => (
             <Link to={item.href} key={i}>
               <motion.div
@@ -34,7 +33,7 @@ export default function Nav1() {
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       exit={{ opacity: 0 }}
-                      transition={{ duration: 0.5 }}
+                      transition={{ duration: 0.3 }}
                       className="-z-10 bg-primary absolute inset-0"
                     />
                   )}
